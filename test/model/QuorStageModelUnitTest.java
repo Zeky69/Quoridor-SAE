2d1beef8b50f9beb897ed66a217fb4611c70bb08
ttherezi <titouan.therezien@edu.univ-fcomte.fr>
2023-04-28 13:21:07 +0200

    UnitTest QuorStageModelUnitTest

---
+++ test/model/QuorStageModelUnitTest.java
@@ -0,0 +1,20 @@
+package model;
+
+import boardifier.model.GameStageModel;
+import boardifier.model.Model;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class QuorStageModelUnitTest {
+
+    @Test
+    public void testConstructQuorStageModel()
+    {
+
+        QuorStageModel quorStageModel = new QuorStageModel("QuorStageModel", new Model());
+
+        Assertions.assertEquals("QuorStageModel", quorStageModel.getName());
+        Assertions.assertEquals(0, quorStageModel.getBoard());
+    }
+}
