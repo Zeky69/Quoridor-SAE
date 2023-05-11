a7f4f0d89f00c18f8da3171d36c1d73c9cc433c8
ttherezi <titouan.therezien@edu.univ-fcomte.fr>
2023-05-11 23:26:05 +0200

    Update PawnUnitTest

--- test/model/PawnUnitTest.java
+++ test/model/PawnUnitTest.java
@@ -1,35 +1,72 @@
 package model;
 
 
-
 import boardifier.model.GameStageModel;
 import boardifier.model.Model;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class PawnUnitTest {
 
-    @Test
-    public void testPawnConstruct()
-    {
+    private Pawn pawn;
+    private GameStageModel gameStageModel;
+
+    @BeforeEach
+    public void setUp() {
         Model model = new Model(); //création du model
         GameStageModel gameStageModel = new QuorStageModel("jeu",model);
         QuorStageModel stageModel;
-        stageModel = (QuorStageModel) gameStageModel;
+        gameStageModel = (QuorStageModel) gameStageModel;
 
-
-        int x =1;
-        int y=2;
-        int player = 3;
-        Pawn pawn = new Pawn(x,y,player,stageModel);
-
-
-        Assertions.assertEquals(1,pawn.getPawnX());
-        Assertions.assertEquals(2,pawn.getPawnY());
-        Assertions.assertEquals(3,pawn.getPlayer());
-
-
+        pawn = new Pawn(0, 0, 1, 8, gameStageModel);
     }
+
+    @Test
+    public void testPawnCoordinates() {
+        assertEquals(0, pawn.getPawnX());
+        assertEquals(0, pawn.getPawnY());
+
+        int[] newCoords = {1, 2};
+        pawn.setPawnXY(newCoords);
+        assertArrayEquals(newCoords, pawn.getPawnXY());
+    }
+
+
+
+    @Test
+    public void testPawnWallCount() {
+        int initialWallCount = pawn.getWallCount();
+        assertEquals(10, initialWallCount);
+
+        pawn.decrementWallCount();
+        assertEquals(initialWallCount - 1, pawn.getWallCount());
+
+        int newWallCount = 5;
+        pawn.setWallCount(newWallCount);
+        assertEquals(newWallCount, pawn.getWallCount());
+    }
+
+    @Test
+    public void testPawnWinY() {
+        assertEquals(8, pawn.getWinY());
+
+        int newWinY = 9;
+        pawn.setWinY(newWinY);
+        assertEquals(newWinY, pawn.getWinY());
+    }
+
+    @Test
+    public void testPawnCopy() {
+        Pawn copy = pawn.copy();
+        assertNotSame(pawn, copy);
+        assertEquals(pawn.getPawnX(), copy.getPawnX());
+        assertEquals(pawn.getPawnY(), copy.getPawnY());
+        assertEquals(pawn.getPlayer(), copy.getPlayer());
+        assertEquals(pawn.getWallCount(), copy.getWallCount());
+        assertEquals(pawn.getWinY(), copy.getWinY());
+    }
+
 }
