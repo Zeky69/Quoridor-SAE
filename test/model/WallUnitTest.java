b2aa0f72b7990346a9daf9bb6414b6e6d0fe7aac
ttherezi <titouan.therezien@edu.univ-fcomte.fr>
2023-05-11 23:25:29 +0200

    Update WallUnitTest

--- test/model/WallUnitTest.java
+++ test/model/WallUnitTest.java
@@ -1,57 +1,98 @@
 package model;
 
-import boardifier.model.GameStageModel;
-import boardifier.model.Model;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+import model.Wall.Direction;
 
 public class WallUnitTest {
+
+
+    private Wall wall;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        boolean[] wallArray = {true, false, true, false};
+        wall = new Wall(wallArray, null);
+    }
+
     @Test
-    public void testConstructWall()
-    {
-        Model model = new Model(); //création du model
-        GameStageModel gameStageModel = new QuorStageModel("jeu",model);
-        QuorStageModel stageModel;
-        stageModel = (QuorStageModel) gameStageModel;
+    public void testGetWall() {
+        boolean[] expected = {true, false, true, false};
+        boolean[] actual = wall.getWall();
+        assertArrayEquals(expected, actual);
+    }
 
-
-        boolean[] wallTest = new boolean[4];
-        wallTest[0] = true;
-        wallTest[1] = false;
-        wallTest[2] = true;
-        wallTest[3] = false;
-        Wall wall1 = new Wall(wallTest,stageModel);
-
-        Assertions.assertArrayEquals(wallTest,wall1.getWall());
-        Assertions.assertTrue(wall1.getWall(Wall.Direction.UP));
-        Assertions.assertFalse(wall1.getWall(Wall.Direction.DOWN));
-        Assertions.assertTrue(wall1.getWall(Wall.Direction.LEFT));
-        Assertions.assertFalse(wall1.getWall(Wall.Direction.RIGHT));
-        Assertions.assertFalse(wall1.isEmpty());
-
+    @Test
+    public void testGetWallDirection() {
+        assertTrue(wall.getWall(Direction.UP));
+        assertFalse(wall.getWall(Direction.DOWN));
+        assertTrue(wall.getWall(Direction.LEFT));
+        assertFalse(wall.getWall(Direction.RIGHT));
     }
 
     @Test
     public void testSetWall() {
-        Model model = new Model(); //création du model
-        GameStageModel gameStageModel = new QuorStageModel("jeu", model);
-        QuorStageModel stageModel;
-        stageModel = (QuorStageModel) gameStageModel;
-
-        boolean[] wallTest = new boolean[]{true, false, true, false};
-
-        Wall wall1 = new Wall(wallTest, stageModel);
-
-        wall1.setWall(Wall.Direction.UP, false);
-
-        Assertions.assertFalse(wall1.getWall(Wall.Direction.UP));
-
-
+        wall.setWall(Direction.UP, false);
+        assertFalse(wall.getWall(Direction.UP));
+        wall.setWall(Direction.DOWN, true);
+        assertTrue(wall.getWall(Direction.DOWN));
+        wall.setWall(Direction.LEFT, false);
+        assertFalse(wall.getWall(Direction.LEFT));
+        wall.setWall(Direction.RIGHT, true);
+        assertTrue(wall.getWall(Direction.RIGHT));
     }
 
     @Test
-    public void testToString(){
-        //TODO TITOU, Faire test de ToString mais voir si c'est possible et quel sortie ça peut etre.
+    public void testIsEmpty() {
+        assertFalse(wall.isEmpty());
+        wall.setWalls(new boolean[4]);
+        assertTrue(wall.isEmpty());
+    }
+
+    @Test
+    public void testCopy() {
+        Wall copy = wall.copy();
+        assertArrayEquals(wall.getWall(), copy.getWall());
+        assertEquals(wall.getGameStageModel(), copy.getGameStageModel());
+        assertNotSame(wall, copy);
+    }
+
+    @Test
+    public void testDirectionToInt() {
+        assertArrayEquals(new int[]{0, -1}, Wall.directionToInt(Direction.UP));
+        assertArrayEquals(new int[]{0, 1}, Wall.directionToInt(Direction.DOWN));
+        assertArrayEquals(new int[]{-1, 0}, Wall.directionToInt(Direction.LEFT));
+        assertArrayEquals(new int[]{1, 0}, Wall.directionToInt(Direction.RIGHT));
+    }
+
+    @Test
+    public void testIntToDirection() {
+        assertEquals(Direction.UP, Wall.intToDirection(0));
+        assertEquals(Direction.DOWN, Wall.intToDirection(1));
+        assertEquals(Direction.LEFT, Wall.intToDirection(2));
+        assertEquals(Direction.RIGHT, Wall.intToDirection(3));
+        assertNull(Wall.intToDirection(4));
+    }
+
+    @Test
+    public void testIsBorder() {
+        assertTrue(Wall.isBorder(new int[]{0, 0}, Direction.UP));
+        assertTrue(Wall.isBorder(new int[]{1, 0}, Direction.UP));
+        assertFalse(Wall.isBorder(new int[]{0, 1}, Direction.UP));
+        assertTrue(Wall.isBorder(new int[]{8, 8}, Direction.DOWN));
+        assertTrue(Wall.isBorder(new int[]{0, 8}, Direction.LEFT));
+        assertFalse(Wall.isBorder(new int[]{1, 8}, Direction.LEFT));
+        assertTrue(Wall.isBorder(new int[]{0, 7}, Direction.LEFT));
+        assertTrue(Wall.isBorder(new int[]{8, 0}, Direction.RIGHT));
+        assertTrue(Wall.isBorder(new int[]{8, 1}, Direction.RIGHT));
+        assertFalse(Wall.isBorder(new int[]{7, 0}, Direction.RIGHT));
     }
 
 }
+
+
