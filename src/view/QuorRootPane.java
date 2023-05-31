e652de5d844f69e7308fee88b632a3e811d44209
Enzo Bellicaud <enzo.bellicaud@edu.univ-fcomte.fr>
2023-05-31 16:17:59 +0200

    create QuorRootPane

---
+++ src/view/QuorRootPane.java
@@ -0,0 +1,28 @@
+package view;
+
+import boardifier.view.RootPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import java.awt.*;
+
+public class QuorRootPane extends RootPane {
+
+    public QuorRootPane() {
+        super();
+    }
+
+    @Override
+    public void createDefaultGroup() {
+        Rectangle frame = new Rectangle(600, 100, Color.LIGHTGREY);
+        Text text = new Text("Playing to Quoridor");
+        text.setFont(new Font(15));
+        text.setFill(Color.BLACK);
+        text.setX(10);
+        text.setY(50);
+        // put shapes in the group
+        group.getChildren().clear();
+        group.getChildren().addAll(frame, text);
+    }
+}
